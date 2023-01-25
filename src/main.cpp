52225dd62ba145e0e6e64f81d04e4cf60d2ad815
Wind <wind@juel-kroyer.dk>
2023-01-25 12:24:57 +0100

    get config from api

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,11 @@ EthernetUDP Udp;
 byte mac[] = { 0XA8, 0X61, 0X0A, 0XAE, 0XA8, 0X56 };
 
 
-
 const char* server = "192.168.1.105";
-int portNumber = 80;
-const char* resource = "/alarm/create.php";
+int portNumber = 8888;
+const char* resource = "measurementconfig/1/config";
 
+String http_response  = "";
 
 
 //offset 1 houer 
@@ -51,7 +51,9 @@ bool IsEthernetOk = 0;
 //timer set up
 unsigned long startTime = 0;
 
-
+//config Setup
+const char* start_time;
+const char* end_time;
 
 bool skipResponseHeaders() {
   // HTTP headers end with an empty line
@@ -59,6 +61,7 @@ bool skipResponseHeaders() {
 
   
   bool ok = client.find(endOfHeaders);
+  
 
   if(!ok) {
     Serial.println("No response or invalid response!");
@@ -86,21 +89,20 @@ bool sendRequest(const char* host, const char* resource) {
   // Reserve memory space for your JSON data
   //StaticJsonDocument<200> jsonBuffer;
   // Build your own object tree in memory to store the data you want to send in the request
-  const size_t capacity = JSON_OBJECT_SIZE(5);
+  const size_t capacity = JSON_OBJECT_SIZE(4);
   DynamicJsonDocument jsonBuffer(capacity);
   
 
-  jsonBuffer["date_time"] = "000000";
   jsonBuffer["isOn"] = "1";
-  jsonBuffer["device_id"] = "1";
-  jsonBuffer["value"] = "NULL";  
+  jsonBuffer["device_id"] = "2";
+  jsonBuffer["value"] = "1";  
   jsonBuffer["measuringUnit_id"] = "5";
 
   //POST request
   Serial.println("Begin POST Request");
 
-  client.println("POST /alarm/create.php HTTP/2.0");
-    Serial.println("POST /alarm/create.php HTTP/2.0");
+  client.println("POST /alarm HTTP/2.0");
+    Serial.println("POST /alarm HTTP/2.0");
 
   client.println("Host: 192.168.1.105");
     Serial.println("Host: 192.168.1.105");
@@ -146,13 +148,78 @@ void normalAlarm()
     lcd.clear();
     lcd.setColorWhite();
     clear = 0;
-    //SendDataToAPI();
     lcd.setRGB(255,0,0);
     lcd.setCursor(0, 0);
     lcd.print(F("Door open"));
     lcd.setCursor(0, 1);
+    SendDataToAPI();
   } 
 
+}
+
+void GetConfig(){
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(F("Getting"));
+  lcd.setCursor(0,1);
+  lcd.print(F("config"));
+
+  if (!client.connect("192.168.1.105", 8888)) {
+    Serial.println(F("Connection failed"));
+    return;
+  }
+
+  // Send HTTP request
+  client.println("GET /measurementconfig/1/config HTTP/2.0");
+  client.println("Host: 192.168.1.105");
+  client.println("Connection: keep-alive");
+  client.println();
+  if (client.println() == 0) {
+    Serial.println(F("Failed to send request"));
+    client.stop();
+    return;
+  }
+
+  // Check HTTP status
+  char status[32] = {0};
+  client.readBytesUntil('\r', status, sizeof(status));
+  // It should be "HTTP/1.0 200 OK" or "HTTP/1.1 200 OK"
+  if (strcmp(status + 9, "200 OK") != 0) {
+    Serial.print(F("Unexpected response: "));
+    Serial.println(status);
+    client.stop();
+    return;
+  }
+
+  // Skip HTTP headers
+  char endOfHeaders[] = "\r\n\r\n";
+  if (!client.find(endOfHeaders)) {
+    Serial.println(F("Invalid response"));
+    client.stop();
+    return;
+  }
+
+  // Allocate the JSON document
+  // Use https://arduinojson.org/v6/assistant to compute the capacity.
+  String payload = client.readString();
+  DynamicJsonDocument doc(payload.length() * 2);
+
+  // Parse JSON object
+  DeserializationError error = deserializeJson(doc, payload);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    client.stop();
+    return;
+  }
+
+  // Extract values
+  start_time = doc["start_time"].as<const char*>();
+  end_time = doc["end_time"].as<const char*>();
+
+  // Disconnect
+  client.stop();
 }
 
 void setup() {
@@ -181,19 +248,23 @@ void setup() {
   //starting up UDP for NTP.
   Udp.begin(8888);
   
-  pinMode(button, INPUT);
+  GetConfig();
+
+  pinMode(button, INPUT_PULLUP);
   timeClient.begin();
   timeClient.update();  
   Serial.println(timeClient.getFormattedTime()); 
   millis();  
+  attachInterrupt(digitalPinToInterrupt(button), GetConfig, FALLING);
 }
 
 void loop() {
   long RangeinCm;
   RangeinCm = ultrasonic.MeasureInCentimeters();
   if(RangeinCm <= 5){
-    if (timeClient.getHours() <= 8 && timeClient.getMinutes() >= 0)
+    if (timeClient.getFormattedTime() < start_time)
     {
+      
       if (openOrclosed == 0)
       {
         openOrclosed = 1;
@@ -201,10 +272,11 @@ void loop() {
       }     
       
     }
-    if (timeClient.getHours() >= 16 && timeClient.getMinutes() >= 0)
+    if (timeClient.getFormattedTime() > end_time)
     {
       if (openOrclosed == 0)
       {
+        Serial.println(F("inde i efter 16"));
         openOrclosed = 1;
         normalAlarm();
       }
